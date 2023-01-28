0eeb18e8314993404d01be72082cd646ec24eb6e
ahsenKas <77925163+ahsenKas@users.noreply.github.com>
2023-01-28 21:12:17 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi " +" "+musteri.Id+  "  "+musteri.Adi +" " +musteri.Soyadi);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi " +musteri.Adi +" " +musteri.Id);
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler Litelendi " +musteri.Id+ " " +musteri.Adi);
+        }
+    }
+}
---
+++ ClassMetotDemo/myMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class myMain
+    {
+        public static void main()
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Ahsen";
+            musteri1.Soyadi = "Kas";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id=2;
+            musteri2.Adi = "Samet";
+            musteri2.Soyadi = "Kas";
+
+            MusteriManager musteriManager1 = new MusteriManager();
+            musteriManager1.MusteriEkle(musteri1);
+            musteriManager1.MusteriEkle(musteri2);
+
+            musteriManager1.MusteriListele(musteri1);
+            musteriManager1.MusteriListele(musteri2);
+
+            musteriManager1.Silme(musteri1);
+            musteriManager1.Silme(musteri2);
+
+
+
+        }
+
+    }
+}
